4774e53a0af7fd19fcec9153140e2dfec338c886
alan.cheung <kahoalan@gmail.com>
2021-04-30 14:47:18 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    // "env": {
+    //   "amd": true
+    // },
+    extends: [
+      // add more generic rulesets here, such as:
+      // 'eslint:recommended',
+      //'plugin:vue/vue3-recommended'
+      'plugin:vue/recommended',
+      "prettier",
+      // "plugin:prettier/recommended",
+      "prettier/vue",
+      // "eslint:recommended"
+    ],
+    rules: {
+      // override/add rules settings here, such as:
+      // 'vue/no-unused-vars': 'error'
+    }
+  }
